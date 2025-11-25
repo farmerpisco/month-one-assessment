59a59efb8aa422244e0bc0a444f797656469968a
Paul Adegoke <farmerpisco@gmail.com>
2025-11-25 12:58:18 +0100

    Created a target group resource for the load balance and its health check

--- main.tf
+++ main.tf
@@ -339,3 +339,20 @@ resource "aws_lb" "lb_tf" {
   ]
   security_groups    = [aws_security_group.alb_sg.id]
 }
+
+resource "aws_lb_target_group" "lb_tg" {
+  name     = "Techcorp-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.tf_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    interval            = 30
+  }
+}
