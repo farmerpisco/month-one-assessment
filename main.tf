b7da679264ac31cf9e057834e92b187fe179c7c6
Paul Adegoke <farmerpisco@gmail.com>
2025-11-25 12:07:21 +0100

    Created a VPC resource

--- main.tf
+++ main.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+resource "aws_vpc" "tf_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Techcorp-vpc"
+  }
+}
