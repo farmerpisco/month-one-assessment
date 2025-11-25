e861976ce75694ea01d4b777d3ab392a2576023a
Paul Adegoke <farmerpisco@gmail.com>
2025-11-25 12:34:22 +0100

    Created a route table resource for the private subnets

--- main.tf
+++ main.tf
@@ -108,3 +108,16 @@ resource "aws_route_table_association" "public_rta2" {
   subnet_id      = aws_subnet.tf_subnet_public2.id
   route_table_id = aws_route_table.tf_rt.id
 }
+
+resource "aws_route_table" "tf_private_rt" {
+  vpc_id = aws_vpc.tf_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
+  tags = {
+    Name = "Techcorp-Private-RT"
+  }
+}
