fab9aa927692402018cb0e09ccbab51a5f803a64
Paul Adegoke <farmerpisco@gmail.com>
2025-11-25 12:53:35 +0100

    Created a Webserver instance resource in each private subnet

--- main.tf
+++ main.tf
@@ -256,3 +256,29 @@ resource "aws_instance" "bastion_host" {
 resource "aws_eip" "bastion_eip" {
   instance = aws_instance.bastion_host.id
 }
+
+resource "aws_instance" "web_server1" {
+  ami             = "ami-0a0ff88d0f3f85a14"
+  instance_type   = "t3.micro"
+  subnet_id       = aws_subnet.tf_subnet_private1.id
+  security_groups = [aws_security_group.wsg.id]
+
+  user_data = file("${path.module}/web_server_setup.sh")
+
+  tags = {
+    Name = "Web-server1"
+  }
+}
+
+resource "aws_instance" "web_server2" {
+  ami             = "ami-0a0ff88d0f3f85a14"
+  instance_type   = "t3.micro"
+  subnet_id       = aws_subnet.tf_subnet_private2.id
+  security_groups = [aws_security_group.wsg.id]
+
+  user_data = file("${path.module}/web_server_setup.sh")
+
+  tags = {
+    Name = "Web-server2"
+  }
+}
