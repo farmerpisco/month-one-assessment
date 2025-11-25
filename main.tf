8fa3cb0667f4d391e4228a80f093880b743f16de
Paul Adegoke <farmerpisco@gmail.com>
2025-11-25 12:13:16 +0100

    Created two public subnets in different availability zones

--- main.tf
+++ main.tf
@@ -20,3 +20,23 @@ resource "aws_vpc" "tf_vpc" {
     Name = "Techcorp-vpc"
   }
 }
+
+resource "aws_subnet" "tf_subnet_public1" {
+  vpc_id     = aws_vpc.tf_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-west-2a"
+
+  tags = {
+    Name = "Techcorp-Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "tf_subnet_public2" {
+  vpc_id     = aws_vpc.tf_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "eu-west-2b"
+
+  tags = {
+    Name = "Techcorp-Public-Subnet-2"
+  }
+}
